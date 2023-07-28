80cc6c835fa00e068d915bf8c07bea8fefbcb94d
Zanottex <gustavozanotto@gmail.com>
2023-07-28 17:17:12 -0300

    Aula dia:28/07

---
+++ src/main/java/com/MundoSenai/Presenca/Controller/C_Pessoa.java
@@ -0,0 +1,24 @@
+package com.MundoSenai.Presenca.Controller;
+
+import com.MundoSenai.Presenca.Service.S_Pessoa;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class C_Pessoa {
+    @GetMapping("/")
+    public String holleWord(){
+        return "Login/login";
+    }
+
+    @PostMapping
+    public String postLogin(@RequestParam ("usuario") String usuario, @RequestParam("senha") String senha){
+        if (S_Pessoa.getPessoaLogin(usuario, senha)== null){
+            return "Login/login";
+        }else{
+            return "teste";
+        }
+    }
+}

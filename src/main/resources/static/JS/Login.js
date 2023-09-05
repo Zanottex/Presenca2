a47873cb0a7fb782eb6ad46d9c991bacaf03d74e
Zanottex <gustavozanotto119@gmail.com>
2023-09-05 17:24:35 -0300

    Dia 05/09

---
+++ src/main/resources/static/JS/Login.js
@@ -0,0 +1,9 @@
+$("btnCadastro").click(function(event)){
+    event.preventDefault();
+    $.get("/cadastro", function(data){
+    $(".container").html(data);
+    history.pushState({},'',"/cadastro");
+    $("#enviar").click(validaEnvio);
+    });
+
+    )}
